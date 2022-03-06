050d9f7bbcce63ce31f93cc86e7772816c7c4fa4
christian julius guillen <juliusguillen@gmail.com>
2022-03-05 21:14:44 -0600

    commit tree a16edcc2ebd009afbfb9cc57607d4f7326bb834d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup - 42);
+};
+
+function distanceFromHqInFeet(pickup) {
+    const blocks = distanceFromHqInBlocks(pickup);
+    return blocks * 264
+};
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264);
+  }
+  function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination)
+    if(feet <= 400){
+        return 0
+    }
+    else if (feet > 400 && feet <= 2000){
+        return ((feet - 400) * .02)
+    }
+    else if (feet > 2000 && feet < 2500 ){
+        return 25 
+    }
+    return "cannot travel that far"
+  }
